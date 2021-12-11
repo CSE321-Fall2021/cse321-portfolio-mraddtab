0c7a4644874f4f8cd31eceb576b36b979ab82250
mraddtab <70644931+mraddtab@users.noreply.github.com>
2021-12-10 19:23:29 -0500

    Add files via upload

--- Project 3/main.cpp
+++ Project 3/main.cpp
@@ -5,21 +5,22 @@ Course: CSE321
 Creation Date: 12/5/2021
 Program Name: Humidity Alarm
 Program Summary: This program is the software required to operate the humidity alarm being designed. 
-                 The humidity alarm is a device that detects the humidity level in a room, and alarms 
-                 the user when the humidity percentage becomes to high or low via the buzzer peripheral.
-                 The buzzer will continue to output noise until levels return to normal or the button B1
-                 on the Nucleo is pressed. The humidity percentage and saftey level will be printed onto 
-                 the LCD. 
+                 The humidity alarm is a device that detects the humidity levels in a room, and alarms 
+                 the user when the humidity percentage becomes too high or low via the buzzer audio module.
+                 The buzzer will continue to output noise until levels return to normal or the user button B1
+                 on the Nucleo is pressed. Once the buzzer is muted, it cannot be unmuted and will remain as such 
+                 until the  state returns to "Fair" or "Ideal" again. This design choice is modelled after 
+                 smoke detectors.The humidity percentage and state will be printed onto the LCD and serial monitor. 
 Inputs: DHT11 humidity data, User button B1
-Outputs: Humidity percentage, and saftey level printed to LCD, Buzzer audio output.
+Outputs: Humidity percentage, and state printed to LCD, Buzzer audio output.
 Implementation Requirements: Watchdog timer, Synchronization technique, Bitwise driver configuration,
                              Critical section protection, Use of threads/tasks, 1 Interrupt.
 Required Materials: Nucleo-L4R5ZI, DHT11 Sensor, Buzzer audio module, 16x2 LCD Display, Wires, Resistors,
-                    Breadboard.
+                    Breadboard, USBA to Micro USB B.
 Additional Info: The buzzer is activated on an actived on an active low signal. 
                  Sampling period of DHT11 must be > 2 seconds.
-                 Pin Mappings:SCL: PB_8, SDA: PB_9, Buzzer: PB_15, Button 1: PC_13, DHT11: PC_6
-                 
+                 Watchdog times out if not kicked within 2.2 seconds.
+                 Pin Mappings:SCL: PB_8, SDA: PB_9, Buzzer: PB_15, Button 1: PC_13, DHT11: PC_6 
 */
 #include "mbed.h"
 #include "DHT11.h"
@@ -31,52 +32,41 @@ Additional Info: The buzzer is activated on an actived on an active low signal.
 #include <stdio.h>
 
 
-int sense_interval = 3000;              // DHT11 Senses every 2.5 seconds.
-const uint32_t watchdog_timeout = 5000; // Watchdog times out every 3.0 seconds.        
-//Threads and Interrupts.
+int sense_interval = 2100;              // DHT11 Senses every 2.1 seconds.
+const uint32_t watchdog_timeout = 3000; // Watchdog times out after 2.2 seconds.        
+
 Thread t_sense;                         // This thread will sense humidity.
-void sense();
 Thread t_display;                       // This thread will update the LCD
-void output();
 Thread t_audio;                         // This thread will control the buzzer.
-void audio();
-
 InterruptIn mute_pressed(PC_13);        // User Button is the mute button.
+void sense();
+void display();
+void audio();
 void mute();
+void initialize_watchdog(int);
 
+Queue<int,1> humidityQue;
+Queue<string,1> display_stateQue;
 DHT11 sensor(PC_6);
 CSE321_LCD LCD(16,2,LCD_5x10DOTS,PB_9,PB_8);
 
-Queue<int,32> humidityQue;
-Queue<string,32> stateQue1;
-bool muted = false;
+bool muted;
 string state;
 int humidity;
-Mutex mux;
 int main()
 {
     LCD.begin();
-    RCC->AHB2ENR |= 0x6;            //Enable ports B and C.
+    initialize_watchdog(watchdog_timeout);
 
+    RCC->AHB2ENR |= 0x6;            //Enable ports B and C.
     GPIOB->MODER &=~(0x800A0000);   //Configure GPIOB Moder: Output (01) for pins 8(SCL), 9(SDA) & 15(Buzzer).
     GPIOB->MODER |= 0x40050000;
-
     GPIOC->MODER &=~(0xC003000);    //Configure GPIOC Moder: Input (00) for pins 6(DHT) & 13(User Button).
-    GPIOB->ODR |= 0x8000;           //Enable output for LCD pins 8 & 9, and disable buzzer noise pin 15.
+    GPIOB->ODR |= 0x8000;           //Disable buzzer noise: PB_15
     GPIOC->IDR |= 0x40;             //Configure DHT11 IDR: PC_6
 
-    // Watchdog is configured to be greater than the time it should required to
-    // read data from the sensor, and update the LCD.
-    Watchdog &watchdog = Watchdog::get_instance();  
-    if(watchdog.start(watchdog_timeout) == true && watchdog.is_running() == true){
-        printf("Watchdog timer started sucessfully \nTimeout initialized to: %ims\n", watchdog.get_timeout());
-    }
-    else{
-        printf("Watchdog timer failed to start.\n");
-    }
-
     t_sense.start(sense);  
-    t_display.start(output);
+    t_display.start(display);
     t_audio.start(audio);
     mute_pressed.rise(mute);
     
@@ -85,8 +75,10 @@ int main()
 /*
 Function Name: sense()
 Function Purpose: This function is executed by the thread t_sense,
-                  and will continuouslly update the global variables
-                  "humidity" and "state". Saftey Levels: 
+                  and will continuouslly read data from the DHT11 
+                  sensor, and update variables "state", and "humidity"
+                  accordingly. 
+                  Saftey Levels: 
                     Ideal: 30% < Humidity% < 60%
                     Fair: 25% <= Humidity% <= 30% OR 60% <= humidity% <=70%.
                     Harmful: 30% > humidity% > 70%, 
@@ -97,84 +89,81 @@ Constraints: Sensor can only sense in intervals greater than 2 seconds.
 void sense(){
     while(true){
         thread_sleep_for(sense_interval);
-        string state;
         sensor.read();
-        humidity = sensor.getHumidity();
-        if(humidity < 30 || humidity < 60 ){
-            state = "Ideal";
+        int humidity_data = sensor.getHumidity();
+        string state_data;
+        if(humidity_data < 30 || humidity_data < 60 ){
+            state_data = "Ideal";
         }
-        else if ((humidity >= 25 && humidity <= 30) || (humidity >= 60 && humidity <= 70)) {
-            state = "Fair";
+        else if ((humidity_data >= 25 && humidity_data <= 30) || (humidity_data >= 60 && humidity_data <= 70)) {
+            state_data = "Fair";
         }
-        else if(humidity < 30 || humidity > 70){
-            state = "Harmful";
+        else if(humidity_data < 30 || humidity_data > 70){
+            state_data = "Harmful";
         }
-        stateQue1.try_put(&state);
-        humidityQue.try_put(&humidity);
+        display_stateQue.try_put(&state_data);
+        humidityQue.try_put(&humidity_data);
     }
 }
 
 /*
-Function Name: output()
+Function Name: display()
 Function Purpose: This function will wait for humidity and state 
                   data from the two Queues and display the humidity 
-                  and state on the LCD display as such:
-                  Humidity Percentage: __ %
-                  Saftey Level: "".
-                  
+                  and state on the LCD display, and serial monitor 
+                  when received. If data isn't received from the 
+                  queue within 3 seconds, the watchdog timer will
+                  reset the program. 
 Inputs: int humidity, string level.
 Outputs: Update LCD everytime data is given to the queue changes.
          Activates buzzer if state == "Harmful", and disables it
          otherwise.
 Constraints: Can only update as fast as sensor is able to sense.
  */
-void output(){ 
+void display(){ 
         while(true){
+            // Wait for data to be sent to both queues.
             osEvent humidityMsg = humidityQue.get(osWaitForever);
-            osEvent stateMsg = stateQue1.get(osWaitForever);
-            LCD.clear();
+            osEvent stateMsg = display_stateQue.get(osWaitForever);
+            
+            //Extract the values and update global variables.
             state = *(string*)stateMsg.value.p;
             humidity = *(int*)humidityMsg.value.p;
+
+            //Construct the strings to display
             string humidity_str = "Humidity: "+ std::to_string(*(int*)humidityMsg.value.p) + "%";
             string state_str = "State: " + *(string*)stateMsg.value.p;
-            const char * humidity_const = humidity_str.c_str();
+            
+            //Convert std::strings to c strings so it can be printed.
+            const char * humidity_const = humidity_str.c_str();     
             const char * state_const = state_str.c_str();
 
+            //Refresh the LCD and print to the serial monitor and LCD.
+            LCD.clear();
             printf("%s\n", humidity_const);
             printf("%s\n\n",state_const);
-
             LCD.print(humidity_const);
             LCD.setCursor(0, 2);
             LCD.print(state_const);
 
-           
+            //Data has been processed, and updated sucessfully, reset the watchdog timer.
             Watchdog::get_instance().kick();
         }   
 }
 
 /*
-Function Name: mute()
-Function Purpose: This function is the ISR for the interrupt
-                  "mute_pressed". If muted == 0, then this 
-                  function will set it to 1. And vice versa.
-Inputs: bool muted; 
-Outputs: muted = 1;
+Function Name: audio()
+Function Purpose: This function continuously monitors the variables "state" and "muted". 
+                  It will activate the buzzer if the state is "Harmful" and isn't muted, 
+                  deactivate it if it is "Fair" or "Ideal", or muted = true. Once muted, 
+                  the buzzer will not output until levels return to fair or normal,
+                  similar to a smoke detector.
+Inputs: bool muted, string state. 
+Outputs: Buzzer activation/deactivaton.
 Constraints:
 */
-void mute(){
-        mute_pressed.disable_irq();
-        osDelay(500);
-        if(state == "Harmful" && muted == false){
-            GPIOB->ODR &=~(0x8000);
-            muted = true;
-        }
-        osDelay(500);
-        mute_pressed.enable_irq();
-}
-
 void audio(){
     while(true){
-         //Buzzer control
             if(state == "Harmful" && muted == false){
                 GPIOB->ODR &=~(0x8000);
             }
@@ -183,10 +172,49 @@ void audio(){
             }
             else if(state == "Fair" || state == "Ideal" ){
                 GPIOB->ODR |= 0x8000;
-                mux.lock();
                 muted = false;
-                mux.unlock();
             }
     }
 }
+/*
+Function Name: mute()
+Function Purpose: This function is the ISR called on the rising edge 
+                  of the interrupt "mute_pressed". It will turn off
+                  the buzzer if state is "Harmful", and isn't currently
+                  muted. 
+Inputs: bool muted; 
+Outputs: muted = 1;
+Constraints: Bounce must be accounted for, as a result there is
+             a delay incorporated, and interrupt requests are
+             disabled.
+*/
+void mute(){
+        mute_pressed.disable_irq();
+        osDelay(500);
+        if(state == "Harmful" && muted == false){
+            muted = true;
+        }
+        osDelay(500);
+        mute_pressed.enable_irq();
+}
 
+
+
+/*
+Function Name: initialize_watchdog()
+Function Purpose: This function is called at the beginning of the program
+                  and initializes the watchdog timer. It will print out
+                  if the timer has been initialized sucessfully or not.
+Inputs: int timeout: Time until watchdog times out
+Outputs: Prints if timer has  initialized sucessfully or not.
+Constraints: 
+*/
+void initialize_watchdog(int timeout){
+    Watchdog &watchdog = Watchdog::get_instance();  
+    if(watchdog.start(timeout) == true && watchdog.is_running() == true){
+        printf("Watchdog timer started sucessfully \nTimeout initialized to: %ims\n\n", watchdog.get_timeout());
+    }
+    else{
+        printf("Watchdog timer failed to start.\n");
+    }
+}
